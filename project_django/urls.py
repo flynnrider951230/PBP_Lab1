0e80f914c353ea7e5e7f342f003fb80946d21d46
shafaleyda <shafaleyda@gmail.com>
2022-09-08 07:30:11 +0700

    Fixing wrongs

--- project_django/urls.py
+++ project_django/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pbpshafa.herokuapp.com.urls')),
+    path('', include('pbpshafa.urls')),
     path('wishlist/', include('wishlist.urls')),
 ]
